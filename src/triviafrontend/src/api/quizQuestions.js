f2480fafbea8d37aaddc8fbfeaf6874a14b69f7b
Nitheesh.S.Kumar <nitheesh.sk@inapp.com>
2020-06-13 15:21:29 +0530

    api host

--- src/triviafrontend/src/api/quizQuestions.js
+++ src/triviafrontend/src/api/quizQuestions.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 import React from 'react'
-const backendUrl = 'http://localhost:3333'
+const backendUrl = 'http://localhost:3333';
 const API = {
-    listquestions:async()=> await axios.get(backendUrl+'/api/checknoder/questions'),
-    checkanswer:async(body)=> await axios.post(backendUrl+'/api/checknoder/questions/checkanswer',body),
-    updateScore:async(body)=> await axios.post(backendUrl+'/api/checknoder/user/update',body),
-    userlogin:async(body)=> await axios.post(backendUrl+'/api/checknoder/user/login',body),
-    listdashboard: async()=> await axios.get(backendUrl+'/api/checknoder/user/dashboard'),
-    createQn: async(body)=> await axios.post(backendUrl+'/api/checknoder/questions/create',body),
-    createUser: async(body)=> await axios.post(backendUrl+'/api/checknoder/admin/user/create',body),
+    listquestions:async()=> await axios.get('/api/checknoder/questions'),
+    checkanswer:async(body)=> await axios.post('/api/checknoder/questions/checkanswer',body),
+    updateScore:async(body)=> await axios.post('/api/checknoder/user/update',body),
+    userlogin:async(body)=> await axios.post('/api/checknoder/user/login',body),
+    listdashboard: async()=> await axios.get('/api/checknoder/user/dashboard'),
+    createQn: async(body)=> await axios.post('/api/checknoder/questions/create',body),
+    createUser: async(body)=> await axios.post('/api/checknoder/admin/user/create',body),
 }
 
 export default API;
